7fd6961d942b530f3089a9c4c47be605968a25b0
Michael Färber <01mf02@gmail.com>
2016-04-28 10:54:31 +0200

    Shorten.

--- RayTracer/Test.hs
+++ RayTracer/Test.hs
@@ -3,35 +3,22 @@ import BackendRayTrace
 import qualified Scene as S
 import Fast_PPM
 
-
-cameraLight :: Diagram Ray
 cameraLight = mm50Camera # translate (V3 0 0 (10)) 
               <> pointLight white # translate (V3 15 1 5)
 
 --example = cylinder # sc blue  # ambient 0.2 # diffuse 0.8 # highlight (Specular 0.5 5) # (transform . aboutX) (90 @@ deg)  
 --  cube #  (transform . aboutY) (45 @@ deg) # sc green # ambient 0.2 # diffuse 0.8 # highlight (Specular 1 50) # translateX 1
 
-boundBox :: Diagram Ray
 boundBox = cube # sc green # scale 1000 # ambient 0.7
 
-baseShape :: Diagram Ray
 baseShape = sphere # sc blue # ambient 0.1 # diffuse 0.6
-shape :: [Diagram Ray] -> Diagram Ray
-shape l = foldl1 (<>) l
 
-addProperty lo l lp = zipWith combine lo lp   
-  where combine o x = o # l x
-transIntervall :: [Double]
-transIntervall = [-6,-3..6]
-propIntervall :: [Double]
-propIntervall = [0, 0.2..1]
-highlightIn = [1,50..]
+props = zipWith (\ s t -> highlight s . translateX t) specs trans
+  where specs = zipWith Specular [0, 0.2 ..] [1, 50 ..]
+        trans = [-6, -3 ..]
 
-specList = map (\(x,y) -> Specular x y) l
-     where l = zipWith (,) propIntervall highlightIn
+shapes = map (baseShape #) $ take 5 props
 
-shapeProp =  addProperty (replicate 5 baseShape) highlight specList
-shapeList = addProperty shapeProp translateX transIntervall
+scene = boundBox <> mconcat shapes <> cameraLight
 
-example = boundBox <> shape shapeList <> cameraLight
-main =  save_ppm "spec.ppm" $ S.simpleRayTracer (renderDia Ray RayOptions example) (500, 500)
+main =  save_ppm "spec.ppm" $ S.simpleRayTracer (renderDia Ray RayOptions scene) (500, 500)
