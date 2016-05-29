bef8ec6a206dc468b57d4c2c29041f89f8358b32
Alex <Ley789@users.noreply.github.com>
2016-05-29 04:32:51 +0200

    changed to hom

--- RayTracer/Blinn_Phong.hs
+++ RayTracer/Blinn_Phong.hs
@@ -7,6 +7,10 @@ import Control.Lens
 import Linear
 import Primitive
 
+-- TODO we still have no inside autside test, so the cos between the normal
+-- and tested vector could be cos (a + 90) if we intersected a primitive from
+-- the inside
+
 -- |Constant ambient colour
 ambientColour :: Colour
 ambientColour = cpromote 1
@@ -16,16 +20,16 @@ ambient = combine ambientColour
 
 -- | Takes the light colour, diffuse coefficient, normalized direction to light
 --   normal vector of the object and returns the diffuse colour reflection.
-diffuse :: Colour -> Double -> V3 Double -> V3 Double -> Colour
+diffuse :: Colour -> Double -> V4 Double -> V4 Double -> Colour
 diffuse c d l n = combine c $ d * influence n l
 
-specular :: Colour -> Double -> V3 Double -> V3 Double -> V3 Double -> Double -> Colour
+specular :: Colour -> Double -> V4 Double -> V4 Double -> V4 Double -> Double -> Colour
 specular c d l n v h = combine c $ d * influence n (bisector v l) **  h  
 
-influence :: V3 Double -> V3 Double -> Double
+influence :: V4 Double -> V4 Double -> Double
 influence n l = max 0 (dot n l)
 
-bisector :: V3 Double -> V3 Double -> V3 Double
+bisector :: V4 Double -> V4 Double -> V4 Double
 bisector v l = normalize $ v + l
 
 combine :: Colour -> Double -> Colour
@@ -40,5 +44,5 @@ blinnPhong li i =
     lp = li ^. lPosition
     v  = negated . normalize $ i ^. ray . _d
     prop = i ^. itTex . property
-    l  = normalize $ lp - normalizePoint (i ^. itPoint)
-    n  = normalizePoint $ i ^. normal
+    l  = normalize $ lp - (i ^. itPoint)
+    n  = i ^. normal
